8e149c3b3e8a013cdcfd904ee32dc714bf4ba5bb
upsea <upsea@upsea.cn>
2016-07-15 11:45:04 +0800

    mids eastmoney csv file added to tushare csv Oked.

--- upsea/Ea_00_BaseClass/midEaController.py
+++ upsea/Ea_00_BaseClass/midEaController.py
@@ -9,8 +9,8 @@ class eaController():
         #mid str to pyTimeStamp
         #self.toPlot = toPlot
         self.ea.toPlot = toPlot
-        timeStampFrom = int(time.mktime(time.strptime(timeFrom, "%Y-%m-%d %H:%M:%S")))
-        timeStampTo   = int(time.mktime(time.strptime(timeTo, "%Y-%m-%d %H:%M:%S")))    
+        timeStampFrom = time.mktime(time.strptime(timeFrom, "%Y-%m-%d %H:%M:%S"))
+        timeStampTo   = time.mktime(time.strptime(timeTo, "%Y-%m-%d %H:%M:%S")) 
         print timeFrom,timeTo
         
         interval = (timeStampTo - timeStampFrom)/phases
@@ -21,8 +21,11 @@ class eaController():
             endTimeStamp = startTimeStamp + interval
             
             #mid pyTimeStamp to datetime
-            timeFromDatetime = dt.datetime.utcfromtimestamp(startTimeStamp)
-            timeToDatetime = dt.datetime.utcfromtimestamp(endTimeStamp)
+            #timeFromDatetime = dt.datetime.utcfromtimestamp(startTimeStamp)
+            #timeToDatetime = dt.datetime.utcfromtimestamp(endTimeStamp)            
+            
+            timeFromDatetime = dt.datetime.fromtimestamp(startTimeStamp)
+            timeToDatetime = dt.datetime.fromtimestamp(endTimeStamp)
             
             self.ea.run(timeFrom = timeFromDatetime,timeTo = timeToDatetime)
 
--- upsea/Ea_11_Dma_pg_01/DMACrossOver.py
+++ upsea/Ea_11_Dma_pg_01/DMACrossOver.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     
     signal = signal.DMACrossOver()
     eaController = midEaController.eaController(signal)
-    eaController.runByPhase('2001-09-25 00:00:00', '2016-07-11 00:00:00', 1,True)
+    eaController.runByPhase('2010-09-27 00:00:00', '2015-07-09 00:00:00', 1,True)
     endRun = time.clock()
     print "run time: %f s" % (endRun - startRun)       
     sys.exit(app.exec_())  
